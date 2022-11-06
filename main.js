dd6fd12ff02d7ec3c603bb283613fb74cc8e420b
Irina-Dehtiarenko <93826301+Irina-Dehtiarenko@users.noreply.github.com>
2022-11-06 11:06:28 +0100

    my solution if challenge #2

--- main.js
+++ main.js
@@ -93,25 +93,48 @@
 
 // JavaScript Fundamentals – Part 2
 
-// Coding Challenge #1
-const calcAverage = (score1, score2, score3) => {
-  return (score1 + score2 + score3) / 3;
-};
+// // Coding Challenge #1
+// const calcAverage = (score1, score2, score3) => {
+//   return (score1 + score2 + score3) / 3;
+// };
 
-const checkWinner = (avgDolphins, avgKoalas) => {
-  console.log(avgDolphins, avgKoalas);
-  if (avgDolphins >= 2 * avgKoalas) {
-    console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas}) `);
-  } else if (avgKoalas >= 2 * avgDolphins) {
-    console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins}) `);
-  } else {
-    console.log("There are no wins");
-  }
-};
+// const checkWinner = (avgDolphins, avgKoalas) => {
+//   console.log(avgDolphins, avgKoalas);
+//   if (avgDolphins >= 2 * avgKoalas) {
+//     console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas}) `);
+//   } else if (avgKoalas >= 2 * avgDolphins) {
+//     console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins}) `);
+//   } else {
+//     console.log("There are no wins");
+//   }
+// };
 
-// Data 1
-checkWinner(calcAverage(44, 23, 71), calcAverage(65, 54, 49));
+// // Data 1
+// checkWinner(calcAverage(44, 23, 71), calcAverage(65, 54, 49));
 
 // Data2
 
 // checkWinner(calcAverage(85, 54, 41), calcAverage(23, 34, 27));
+
+// Challenge #2
+
+// const bill = 275;
+// const bill = 40;
+// const bill = 430;
+
+// const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+// console.log(
+//   `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
+// );
+
+const calcTip = (bill) =>
+  bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+const bills = [125, 555, 44];
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(total);
